f4fec494de40b4ddd102e3d90484253c47c90c10
Abram Chavez <abram.chavez820@gmail.com>
2021-06-24 19:58:25 -0500

    attempted to make button function with inputs val

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,31 @@ $(document).ready(handleReady);
 
 function handleReady() {
   console.log("jquery is loaded!")
+  //Abrams Code
+// Get value on button click and show alert
+  $("#myBtn").on('click', submitAnswer(){
+  })
 }
+
+  //created global array with nested object
+const answers = [];
+
+//making function to push inputs into employeeList array & grab employee details from the input fields..
+function submitAnswer() {
+  
+let newAnswer= {
+   fName: $('#joseInput').val(),
+   lName: $('#shawnInput').val(),
+   idNumber: $('#KongInput').val(),
+   jobTitle: $('#AbramInput').val(),
+    }
+
+    //clear the input fields. 
+$('#joseInput').val('')
+$('#shawnInput').val('')
+$('#KongInput').val('')
+$('#AbramInput').val('')
+
+answers.push(submitAnswer);
+
+  }
